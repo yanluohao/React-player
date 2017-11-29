38988d9ff96f85582898d95e1ab853e236565d19
ylh <345072986@qq.com>
2017-11-29 18:20:25 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Head from "./components/header/header";
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                
+            </div>
+        );
+    }
+}
+
+export default App;
